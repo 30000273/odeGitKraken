adf429de4724da5a2d3e34dd632bae8a145a130c
30000273 <30000273@sbstudents.org>
2024-10-30 14:07:24 -0400

    1000 threads stored in array

---
+++ partOne/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static Thread[] threadArr = new Thread[1000];
+    
+}
+
